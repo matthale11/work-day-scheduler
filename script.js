c483691a44a338034112eaa4f1b65ade61a935a5
Matt Hale <mhale@factharbor.com>
2021-03-27 13:37:00 -0400

    added save icon and set column widths and other formatting

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Use the ID for currentDay and moment to display the day of the week, month, and day of the month
+var currentHour = moment().format("HH");
+console.log(currentHour);
+
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // 
