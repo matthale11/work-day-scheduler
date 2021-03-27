2091bb640982b7e3a6a19f6f751fbecdd499a7ff
Matt Hale <mhale@factharbor.com>
2021-03-27 16:13:39 -0400

    save textarea appointments to local storage

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 // Use the ID for currentDay and moment to display the day of the week, month, and day of the month
 var currentHour = moment().format("HH");
-console.log(currentHour);
-var nineAM = 9;
-var tenAM = 10;
-var elevenAM = 11;
+var nineAm = 9;
+var tenAm = 10;
+var elevenAm = 11;
 var noon = 12;
-var onePM = 13;
-var twoPM = 14;
-var threePM = 15;
-var fourPM = 16;
-var fivePM =17;
-console.log(onePM);
+var onePm = 13;
+var twoPm = 14;
+var threePm = 15;
+var fourPm = 16;
+var fivePm =17;
+
+// Create variables for save buttons
+var nineAmSave = $("#9am-save");
+var tenAmSave = $("#10am-save");
+var elevenAmSave = $("#11am-save");
+var noonSave = $("#noon-save");
+var onePmSave = $("#1pm-save");
+var twoPmSave = $("#2pm-save");
+var threePmSave = $("#3pm-save");
+var fourPmSave = $("#4pm-save");
+var fivePmSave = $("#5pm-save");
 
 // Displays the current day, month, and day of the month in header
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // For each hour, change the class from the CSS stylesheet to past, present, or future
 // 9 AM
-if(currentHour > nineAM) {
+if(currentHour > nineAm) {
     $("#9am").addClass("past");
-} else if (currentHour == nineAM) {
+} else if (currentHour == nineAm) {
     $("#9am").addClass("present");
 } else {
     $("#9am").addClass("future");
 }
 // 10 AM
-if(currentHour > tenAM) {
+if(currentHour > tenAm) {
     $("#10am").addClass("past");
-} else if (currentHour == tenAM) {
+} else if (currentHour == tenAm) {
     $("#10am").addClass("present");
 } else {
     $("#10am").addClass("future");
 }
 // 11 AM
-if(currentHour > elevenAM) {
+if(currentHour > elevenAm) {
     $("#11am").addClass("past");
-} else if (currentHour == elevenAM) {
+} else if (currentHour == elevenAm) {
     $("#11am").addClass("present");
 } else {
     $("#11am").addClass("future");
@@ -49,42 +58,98 @@ if(currentHour > noon) {
     $("#noon").addClass("future");
 }
 // 1 PM
-if(currentHour > onePM) {
+if(currentHour > onePm) {
     $("#1pm").addClass("past");
-} else if (currentHour == onePM) {
+} else if (currentHour == onePm) {
     $("#1pm").addClass("present");
 } else {
     $("#1pm").addClass("future");
 }
 // 2 PM
-if(currentHour > twoPM) {
+if(currentHour > twoPm) {
     $("#2pm").addClass("past");
-} else if (currentHour == twoPM) {
+} else if (currentHour == twoPm) {
     $("#2pm").addClass("present");
 } else {
     $("#2pm").addClass("future");
 }
 // 3 PM
-if(currentHour > threePM) {
+if(currentHour > threePm) {
     $("#3pm").addClass("past");
-} else if (currentHour == threePM) {
+} else if (currentHour == threePm) {
     $("#3pm").addClass("present");
 } else {
     $("#3pm").addClass("future");
 }
 // 4 PM
-if(currentHour > fourPM) {
+if(currentHour > fourPm) {
     $("#4pm").addClass("past");
-} else if (currentHour == fourPM) {
+} else if (currentHour == fourPm) {
     $("#4pm").addClass("present");
 } else {
     $("#4pm").addClass("future");
 }
 // 5 PM
-if(currentHour > fivePM) {
+if(currentHour > fivePm) {
     $("#5pm").addClass("past");
-} else if (currentHour == fivePM) {
+} else if (currentHour == fivePm) {
     $("#5pm").addClass("present");
 } else {
     $("#5pm").addClass("future");
 }
+
+// Add function to render existing appointments from local storage
+// function renderAppointments () {
+//     var nineAmText = localStorage.getItem("9am");
+//     if (!nineAmText) {
+//         return;
+//     }
+
+// }
+
+// Add event listeners: When clicking the save button, we save the cell contents to local storage
+nineAmSave.click(function(event) {
+    event.preventDefault();
+    var nineAmText = $("#9am-text").val();
+    localStorage.setItem("9am", nineAmText);
+});
+tenAmSave.click(function(event) {
+    event.preventDefault();
+    var tenAmText = $("#10am-text").val();
+    localStorage.setItem("10am", tenAmText);
+});
+elevenAmSave.click(function(event) {
+    event.preventDefault();
+    var elevenAmText = $("#11am-text").val();
+    localStorage.setItem("11am", elevenAmText);
+});
+noonSave.click(function(event) {
+    event.preventDefault();
+    var noonText = $("#noon-text").val();
+    localStorage.setItem("noon", noonText);
+});
+onePmSave.click(function(event) {
+    event.preventDefault();
+    var onePmText = $("#1pm-text").val();
+    localStorage.setItem("1pm", onePmText);
+});
+twoPmSave.click(function(event) {
+    event.preventDefault();
+    var twoPmText = $("#2pm-text").val();
+    localStorage.setItem("2pm", twoPmText);
+});
+threePmSave.click(function(event) {
+    event.preventDefault();
+    var threePmText = $("#3pm-text").val();
+    localStorage.setItem("3pm", threePmText);
+});
+fourPmSave.click(function(event) {
+    event.preventDefault();
+    var fourPmText = $("#4pm-text").val();
+    localStorage.setItem("4pm", fourPmText);
+});
+fivePmSave.click(function(event) {
+    event.preventDefault();
+    var fivePmText = $("#5pm-text").val();
+    localStorage.setItem("5pm", fivePmText);
+});
